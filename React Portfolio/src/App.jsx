a7a5661d45f762775a9e859a1122579da8e50706
Tarun Parajuli <Tarunparajuli@gmail.com>
2024-03-14 20:48:33 -0400

    renamed

--- React Portfolio/src/App.jsx
+++ React Portfolio/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header.jsx';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import AboutMe from './pages/AboutMe';
+import AboutMe from './pages/AboutMe.jsx/index.js';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
 import NotFound from './pages/NotFound';
