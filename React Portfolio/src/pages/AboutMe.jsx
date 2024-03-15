f7c0884bdccd0d105052ee301e3acf71c8f293dd
Tarun Parajuli <Tarunparajuli@gmail.com>
2024-03-14 20:57:58 -0400

    adoubt me bug

---
+++ React Portfolio/src/pages/AboutMe.jsx
@@ -0,0 +1,13 @@
+// In AboutMe.jsx
+import React from 'react';
+
+const AboutMe = () => {
+  // Your component code
+  return (
+    <div>
+      {/* Your JSX here */}
+    </div>
+  );
+};
+
+export default AboutMe;
