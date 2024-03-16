fbb769b18f0c1b80ff46fc37f71c321b624be336
Tarun Parajuli <Tarunparajuli@gmail.com>
2024-03-16 03:06:51 -0400

    renamed files

---
+++ React Portfolio/src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+// Inside Contact.jsx
+import React from 'react';
+
+function Contact() {
+  // Your JSX code for the contact component
+  return (
+    <div>Contact content here</div>
+  );
+}
+
+export default Contact;
